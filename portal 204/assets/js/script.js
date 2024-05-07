671cd424950ff3692ee5839a878a71ecbb55f2f2
Iury-Gabriel <157987319+Iury-Gabriel@users.noreply.github.com>
2024-05-06 21:25:53 -0300

    atualizando infos

--- portal 204/assets/js/script.js
+++ portal 204/assets/js/script.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const quintaHorario = document.querySelector('.quintaHorario');
     const sextaHorario = document.querySelector('.sextaHorario');
 
-    var currentPage = window.location.pathname.split("/").pop(); // Obtém o nome do arquivo atual
+    var currentPage = window.location.pathname.split("/").pop(); 
 
     // Adiciona a classe 'active' ao link da página atual
     var links = document.querySelectorAll(".navBarDesktop ul li a, .navBarMobile ul li a");
